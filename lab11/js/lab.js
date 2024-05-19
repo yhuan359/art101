7cd5953eef481198bc51d61d8115b29a532781e7
yhuan359 <166178438+yhuan359@users.noreply.github.com>
2024-05-18 17:11:11 -0700

    Add files via upload

---
+++ lab11/js/lab.js
@@ -0,0 +1,27 @@
+/*
+   lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Yifeng Huang
+   Date: 2024
+*/
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+// click listener for button
+$("#submit").click(function() {
+  // get value of input field
+  const userName = $("#user-name").val();
+
+  // now let's sort it
+  userNameSorted = sortString(userName);
+
+  // append a new div to our output div
+  $("#content").append('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
+
