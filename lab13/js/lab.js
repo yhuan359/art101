457dc2763b7fb701cb4d9394b3dd23532637cf67
yhuan359 <166178438+yhuan359@users.noreply.github.com>
2024-05-26 22:45:20 -0700

    Add files via upload

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -5,14 +5,11 @@
  * License: Public Domain
  */
 
-maxFactors = 4;
+maxFactors = 3;
 
 outputEl = document.getElementById("output");
 
-// get the values from the webpage and write them in an object
-// this expects to have input fields with ids num0, text0, num1, text1, etc
-// returns an object that looks like this:
-//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
+// get the values from the inputs in the webpage and write them in an object
 function getFactorObj() {
     var factorObj = {};
     for (var factor=0; factor<maxFactors; factor++) {
@@ -21,7 +18,7 @@ function getFactorObj() {
         numValue = document.getElementById(numId).value;
         textValue = document.getElementById(textId).value;
         console.log(factor + ") num:", numValue, "text:", textValue)
-        // if either value is blank, don't use it
+        // if there is no value, do not use it
         if (numValue && textValue) {
             factorObj[numValue] = textValue;
         }
@@ -29,60 +26,29 @@ function getFactorObj() {
     return factorObj;
 }
 
+// create the output line
 function outputToPage(str) {
     newEl = document.createElement("p");
     newEl.innerHTML = str;
     outputEl.appendChild(newEl);
 }
 
-  
-    //   // For numbers which are multiples of both 3 and 5, print “FizzBuzz”
-    //   if ((i % 3 == 0) && (i % 5 == 0) && (i % 7 == 0)) {
-    //     console.log("FizzBuzzBoom");
-    //   } 
-    //   // For numbers which are multiples of both 3 and 5, print “FizzBuzz”
-    //   if ((i % 3 == 0) && (i % 5 == 0)) {
-    //     console.log("FizzBuzz");
-    //   } 
-    //   // For numbers which are multiples of both 3 and 5, print “FizzBuzz”
-    //   if ((i % 3 == 0) && (i % 7 == 0)) {
-    //     console.log("FizzBoom");
-    //   }
-    //   // For numbers which are multiples of both 3 and 5, print “FizzBuzz”
-    //   if ((i % 5 == 0) && (i % 7 == 0)) {
-    //     console.log("BuzzBoom");
-    //   }
-    //   // If the number is a multiple of 3, print “Fizz” instead of the number.
-    //   else if (i % 3 == 0) {
-    //     console.log("Fizz");
-    //   } 
-    //   // If the number is a multiple of 5, print “Buzz” instead of the number.
-    //   else if (i % 5 == 0) {
-    //     console.log("Buzz");
-    //   }
-    //   // If the number is a multiple of 7, print “Boom” instead of the number.
-    //   else if (i % 7 == 0) {
-    //     console.log("Boom");
-    //   }
-    //   else {
-    //     console.log(i);
-    //   }
-      
-function fizzBuzzBoom(maxNums, factorObj) {
-    // iterate over all of out numbers
+
+function ThreeFactors(maxNums, factorObj) {
+    // loop over all of output numbers
     for (var num=0; num<maxNums; num++) {
         debugger;
-        // reset output string
+        // initial the output string
         var outputStr = "";
-        // iterate over the factors we got from the html
+        // iterate over the factors from the html
         for (var factor in factorObj) {
-            // check to see if this num is a multiple of factor
+            // if the num is a multiple of factors, add the text to output string.
+            // otherwise, print as it.
             if (num % factor == 0) {
-                // if yes, than add the text to output string
                 outputStr += factorObj[factor];
             }
         }
-        // now if we have words in outputStr, format it like this " - FizzBuzz!"
+        // format the output string to " - xxxxxxx!"
         if (outputStr) {
             outputStr = " - " + outputStr + "!";
         }
@@ -109,6 +75,6 @@ document.getElementById("submit").addEventListener("click", function() {
     }
     // clear error if there is one
     outputEl.innerHTML = "";
-    fizzBuzzBoom(max, factorObj);
+    ThreeFactors(max, factorObj);
     outputEl.classList.add("cols");
 })
